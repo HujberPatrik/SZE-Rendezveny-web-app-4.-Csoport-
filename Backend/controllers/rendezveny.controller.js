6edf613a2114a5c5fffbcf13271c734fa5832384
HalaszPeter01 <halasz.peter@hallgato.sze.hu>
2025-02-26 23:14:56 +0100

    Kérvény hozzáadás és módosítás route hozzáadva. sql fájl frissítve

--- Backend/controllers/rendezveny.controller.js
+++ Backend/controllers/rendezveny.controller.js
@@ -1,7 +1,7 @@
 const pool = require('../config/db');
 
 
-exports.getAllKerveny = async (req, res) => {
+const getAllKerveny = async (req, res) => {
     try {
       const result = await pool.query('SELECT * FROM kerveny');
       res.status(200).json(result.rows);
@@ -9,3 +9,40 @@ exports.getAllKerveny = async (req, res) => {
       res.status(400).json({ error: error.message });
     }
 };  
+const insertKerveny = async (req,res) => {
+  const {nev,helyszin,email,statusz,telefon,letszam} = req.body;
+
+  try {
+    const query = 'INSERT INTO kerveny(nev, helyszin, email, statusz,telefon,letszam) VALUES($1, $2, $3, $4, $5, $6)';
+    const values = [nev, helyszin, email,statusz,telefon,letszam];
+
+    await pool.query(query, values);
+    res.status(200).json({msg: "Sikeres hozzáadás"});
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const updateKerveny = async (req, res) => {
+  const { id, nev, email, statusz, telefon, letszam } = req.body;
+
+  try {
+      const query = `UPDATE kerveny 
+                     SET nev = $1, email = $2, statusz = $3, telefon = $4, letszam = $5 
+                     WHERE id = $6 
+                     RETURNING *`;
+      const values = [nev, email, statusz, telefon, letszam, id];
+
+      const result = await pool.query(query, values);
+      if (result.rowCount === 0) {
+          res.status(404).json({ error: 'Data not found' });
+      } else {
+          res.status(200).json(result.rows[0]);
+      }
+  } catch (err) {
+      console.error('Error updating data:', err.message);
+      res.status(400).json({ error: err.message });
+  }
+};
+
+module.exports = {getAllKerveny, insertKerveny, updateKerveny  };
--- Backend/routes/rendezveny.route.js
+++ Backend/routes/rendezveny.route.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const Rendezveny = require('../controllers/rendezveny.controller.js');
 
 router.get('/kervenyek', Rendezveny.getAllKerveny);
-
-
+router.post('/kervenyHozzaad',Rendezveny.insertKerveny);
+router.patch('/kervenyModosit',Rendezveny.updateKerveny);
 module.exports = router;
 
     
